42dcd2b368da19c93828c97ba55891f080facb06
Alain Knaff <alain@knaff.lu>
2018-12-09 23:45:37 +0000

    Version 4.0.23

--- patchlevel.c
+++ patchlevel.c
@@ -18,10 +18,10 @@
 #include "sysincludes.h"
 #include "msdos.h"
 
-const char *mversion="4.0.21";
+const char *mversion="4.0.23";
 
 /* Multiple releases on same day should be marked with (b), (cd), (d) after
  * date string below */
-const char *mdate = "November 24th, 2018";
+const char *mdate = "December 9th, 2018";
 
-const char *mformat_banner = "MTOO4021";
+const char *mformat_banner = "MTOO4023";
